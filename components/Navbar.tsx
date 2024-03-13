b5ce3111e715f25559f2a61ea3e07cf30f8abbcb
Keddy <bokelusi@gmail.com>
2024-03-13 18:48:48 +0100

    this is the first commit for this side project i spotted on some website. it is an admin dashboard basically

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiOutlineShopping } from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div className='w-full justify-end'>
+        <p className="font">this is the navbar</p>
+        <button className="user"><AiOutlineShopping/></button>
+    </div>
+  )
+}
+
+export default Navbar

6048ee775c339da90da33b3fac279924113701d1
Keddy <bokelusi@gmail.com>
2024-03-14 23:21:59 +0100

    sunflower

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { FaUserCircle } from "react-icons/fa";
+import { TbUserSquareRounded } from "react-icons/tb";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
 import { FaGear } from "react-icons/fa6";
@@ -15,14 +15,20 @@ const Navbar = () => {
     <div className={styles.navBar}>
         <RxHamburgerMenu fontSize={25}/>
         <p className="font">Admin Dashboard</p>
-        <div className={styles.search}><input type="text" placeholder='Search Here...'/>&nbsp;<CiSearch fontSize={21} /></div>
-        <div><FaGear fontSize={24}/></div>
+        <div className={styles.search}><input type="text" placeholder='Search Here...' className='input' />&nbsp;<CiSearch fontSize={21} /></div>
+        <div><FaGear fontSize={21}/></div>
         <div className={styles.icons}>
-          <IoGiftOutline />
-          <LuMessagesSquare />
-          <IoIosNotifications />
+          <button>
+            <IoGiftOutline fontSize={20} />
+          </button>
+          <button>
+            <IoIosNotifications fontSize={20} />
+          </button>
+          <button>
+            <LuMessagesSquare fontSize={20}/>
+          </button>
         </div>
-        <div className={styles.user}><a href="/"><FaUserCircle fontSize={25}/></a></div>
+        <div className={styles.user}><a href="/"><TbUserSquareRounded fontSize={25}/> David</a></div>
     </div>
   )
 }

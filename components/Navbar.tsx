26a458a09adba8b6a92d43c8b179edb4cdde60b0
Keddy <bokelusi@gmail.com>
2024-03-14 23:59:54 +0100

    this is the final commit. done with navbar for now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { TbUserSquareRounded } from "react-icons/tb";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
-import { FaGear } from "react-icons/fa6";
+import { GoGear } from "react-icons/go";
 import { IoGiftOutline } from "react-icons/io5";
 import { LuMessagesSquare } from "react-icons/lu";
 import { IoIosNotifications } from "react-icons/io";
@@ -15,17 +15,17 @@ const Navbar = () => {
     <div className={styles.navBar}>
         <RxHamburgerMenu fontSize={25}/>
         <p className="font">Admin Dashboard</p>
-        <div className={styles.search}><input type="text" placeholder='Search Here...' className={styles.input} />&nbsp;<CiSearch fontSize={21} /></div>
-        <div><FaGear fontSize={21}/></div>
+        <div className={styles.search}><input type="text" placeholder='Search Here...' className={styles.input} /><CiSearch fontSize={21} /></div>
+        <div className={styles.gear}><GoGear fontSize={21}/></div>
         <div className={styles.icons}>
-          <button>
-            <IoGiftOutline fontSize={20} />
-          </button>
           <button>
             <IoIosNotifications fontSize={20} />
           </button>
           <button>
             <LuMessagesSquare fontSize={20}/>
+          </button>
+          <button>
+            <IoGiftOutline fontSize={20} />
           </button>
         </div>
         <div className={styles.user}><a href="/"><TbUserSquareRounded fontSize={25}/> David</a></div>
